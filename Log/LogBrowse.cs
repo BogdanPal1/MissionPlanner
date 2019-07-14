6a77df9ac247f7fba4aa61f93709ecddc48c4947
Alexander Remizov <avremizov@gmail.com>
2019-07-14 21:17:41 +0300

    skip AP_Follow messages

--- Log/LogBrowse.cs
+++ Log/LogBrowse.cs
@@ -1531,6 +1531,8 @@ namespace MissionPlanner.Log
                             continue;
 
                         string mode = item.items[index].ToString().Trim();
+                        if (mode.StartsWith("Foll"))
+                            continue;
                         if (top)
                         {
                             var temp = new TextObj(mode, a, zg1.GraphPane.YAxis.Scale.Min, CoordType.AxisXYScale,
